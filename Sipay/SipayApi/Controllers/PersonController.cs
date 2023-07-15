44c8ffbba8f4c9fd657a30004b59b2fd56d84203
aleyna <aleynaebrt@gmail.com>
2023-07-15 18:34:17 +0300

    Fluent Validation ile düzenlendi.

---
+++ Sipay/SipayApi/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using SipayApi.Models;
+using SipayApi.Validator;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SipayApi.Controllers;
+
+
+[ApiController]
+[Route("sipy/api/[controller]")]
+
+public class PersonController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Post([FromBody] Person person)
+    {
+        var validator = new PersonValidator();
+        var result = validator.Validate(person);
+
+        if (result.IsValid)
+        {
+            return Ok(person);
+        }
+
+        var errorMessages = result.Errors.Select(x => x.ErrorMessage).ToList();
+        return BadRequest(errorMessages);
+    }
+
+}
---
+++ Sipay/SipayApi/Validator/PersonValidator.cs
@@ -0,0 +1,58 @@
+﻿using FluentValidation;
+using SipayApi.Models;
+
+namespace SipayApi.Validator
+{
+    public class PersonValidator:AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(x => x.Name)
+               .NotEmpty().WithMessage("Staff person name is required.")
+               .Length(5, 100).WithMessage("The staff person's name should have a character count ranging from 5 to 100 characters.");
+
+            RuleFor(x => x.Lastname)
+                .NotEmpty().WithMessage("Staff person lastname is required.")
+                .Length(5, 100).WithMessage("The staff person's lastname should have a character count ranging from 5 to 100 characters.");
+
+            RuleFor(x => x.Phone)
+                .NotEmpty().WithMessage("Staff person phone number is required.")
+                .Matches("^\\d{10}$").WithMessage("Staff person phone number should contain only numerical digits.");
+
+            RuleFor(x => x.AccessLevel)
+                .NotEmpty().WithMessage("Staff person access level is required.")
+                .InclusiveBetween(1, 5).WithMessage("The access level of the staff person to the system should be within the range of 1 to 5.");
+
+            RuleFor(x => x.Salary)
+                .NotEmpty().WithMessage("Salary level is required.")
+                .InclusiveBetween(5000, 50000).WithMessage("Staff person salary must be between 5000 and 50000.")
+            .Custom((salary, context) =>
+            {
+                var person = (Person)context.InstanceToValidate;
+                switch (person.AccessLevel)
+                {
+                    case 1:
+                        if (salary > 10000)
+                            context.AddFailure("Salary cannot be greater than 10000 for Access Level 1.");
+                        break;
+                    case 2:
+                        if (salary > 20000)
+                            context.AddFailure("Salary cannot be greater than 20000 for Access Level 2.");
+                        break;
+                    case 3:
+                        if (salary > 30000)
+                            context.AddFailure("Salary cannot be greater than 30000 for Access Level 3.");
+                        break;
+                    case 4:
+                        if (salary > 40000)
+                            context.AddFailure("Salary cannot be greater than 40000 for Access Level 4.");
+                        break;
+                    default:
+                        context.AddFailure("Invalid Access Level.");
+                        break;
+                }
+            });
+
+        }
+    }
+}
